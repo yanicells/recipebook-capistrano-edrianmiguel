0a8e248bc6f955b04e3a3c8c8382119ae8fb4674
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-02-09 21:40:50 +0800

    fix: implement proper reading from ctx for templates

--- ledger/views.py
+++ ledger/views.py
@@ -101,30 +101,38 @@ def recipe_1(request):
 
 def recipe_2(request):
     ctx = {
-    "name": "Recipe 1",
+    "name": "Recipe 2",
     "ingredients": [
         {
-            "name": "tomato",
-            "quantity": "3pcs"
+            "name": "garlic",
+            "quantity": "1 head"
         },
         {
             "name": "onion",
             "quantity": "1pc"
         },
         {
-            "name": "pork",
-            "quantity": "1kg"
+            "name": "vinegar",
+            "quantity": "1/2cup"
         },
         {
             "name": "water",
-            "quantity": "1L"
+            "quantity": "1 cup"
         },
         {
-            "name": "sinigang mix",
-            "quantity": "1 packet"
+            "name": "salt",
+            "quantity": "1 tablespoon"
+        },
+        {
+            "name": "whole black peppers",
+            "quantity": "1 tablespoon"
+        },
+        {
+            "name": "pork",
+            "quantity": "1 kilo"
         }
     ],
-    "link": "/recipe/1"
+    "link": "/recipe/2"
 }
     
     return render(request, 'ledger/recipe_detail.html', ctx)
