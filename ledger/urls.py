75575c5623e7be278cf59c05659a35fcb66776cd
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-02-09 21:36:16 +0800

    feat: add urls

---
+++ ledger/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import recipe_list, recipe_1, recipe_2
+
+app_name = "ledger"
+
+urlpatterns = [
+    path('recipes/list', recipe_list, name='recipe-list'),
+    path('recipe/1', recipe_1, name='recipe-1'),
+    path('recipe/2', recipe_2, name='recipe-2'),
+]
