137a48a40ad9900010d355c9a187f2fb802844f4
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-02-19 18:55:57 +0800

    feat: add inlines for admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, RecipeIngredient
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
+    inlines = [RecipeIngredientInline]
 
 admin.site.register(Recipe, RecipeAdmin)
