77b62262b6b95759c5fe4b58375c483a5d2c072e
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-02-19 18:50:32 +0800

    feat: update urls

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import recipe_list, recipe_1, recipe_2
+from .views import recipe_detail, recipe_list
 
 app_name = "ledger"
 
 urlpatterns = [
     path('recipes/list', recipe_list, name='recipe-list'),
-    path('recipe/1', recipe_1, name='recipe-1'),
-    path('recipe/2', recipe_2, name='recipe-2'),
+    path('recipe/<int:pk>/', recipe_detail, name='recipe-detail'),
 ]
