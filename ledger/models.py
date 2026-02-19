466a636947aad0231e81e762379f864d8affb5d8
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-02-19 18:41:18 +0800

    feat: add models for recipe, ingredient, recipeingredient

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    def get_absolute_self_url(self):
+        return reverse('ledger:ingredient-detail', args=[str(self.pk)])
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    def get_absolute_self_url(self):
+        return reverse('ledger:recipe-detail', args=[str(self.pk)])
+    
+class RecipeIngredient(models.Model):
+    quantity = models.FloatField()
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+
+    def __str__(self):
+        return '{} {} of {}'.format(self.quantity, self.ingredient.name, self.recipe.name)
