dae67bd9aabe9c58eb8fd7d5c8d4d607ed756608
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-02-19 18:54:50 +0800

    feat: add admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+admin.site.register(Recipe, RecipeAdmin)
