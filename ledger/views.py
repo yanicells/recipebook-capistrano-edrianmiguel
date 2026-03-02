44dc47376522a55f1b7d0a6e8522483b287784dd
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-03-02 18:20:33 +0800

    feat: fix pep8 issues

--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
+
 from .models import Recipe
 
 
--- recipebook/settings.py
+++ recipebook/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
