fd81e18f83f5e7e74b10065023417e8f52f28ddb
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-03-02 12:12:59 +0800

    feat: add Profile and extend Recipe with author and timestamps

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.validators import MinLengthValidator 
 from django.db import models
 from django.urls import reverse
 
@@ -12,8 +14,28 @@ class Ingredient(models.Model):
         return reverse('ledger:ingredient-detail', args=[str(self.pk)])
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    name = models.CharField(max_length=50)
+    short_bio = models.TextField(validators=[MinLengthValidator(256)])
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name}'
