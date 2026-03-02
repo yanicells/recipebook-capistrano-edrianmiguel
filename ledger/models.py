abd22989add8fa280102d0a3abf52acf54c1e6af
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-03-02 14:51:59 +0800

    feat: update imports for models

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,5 @@
-from django.core.validators import MinLengthValidator
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.urls import reverse
 
