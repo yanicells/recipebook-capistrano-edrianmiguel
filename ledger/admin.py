ac029b4899a2569b185570acb025394171d8f783
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-03-02 12:40:05 +0800

    feat: update admin for profile and user

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-
-from .models import Ingredient, Recipe, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Ingredient, Profile, Recipe, RecipeIngredient
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
 
 
 class RecipeAdmin(admin.ModelAdmin):
@@ -19,7 +29,8 @@ class IngredientAdmin(admin.ModelAdmin):
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
 
-
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
