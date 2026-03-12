a8eb923e5cdc83b00bd1eaee2407b70831d9d960
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-03-12 21:56:49 +0800

    feat: update settings

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -127,3 +127,6 @@ LOGIN_REDIRECT_URL = '/recipes/list'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
