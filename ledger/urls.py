d78addd47feaa79ffd8bd38e161dfb49b7e98622
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-03-12 22:48:29 +0800

    fix: change url from add-image to add_image

--- ledger/urls.py
+++ ledger/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('recipes/list', views.RecipeListView.as_view(), name='recipe-list'),
     path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe-detail'),
     path('recipe/add', views.RecipeCreateView.as_view(), name='recipe-add'),
-    path('recipe/<int:pk>/add-image',
+    path('recipe/<int:pk>/add_image',
          views.RecipeImageCreateView.as_view(), name='recipe-add-image')
 ]
